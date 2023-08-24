380732789f1b736a79c064e6be6d392e01352326
SebastianGs20 <118822817+SebastianGs20@users.noreply.github.com>
2023-08-24 11:04:43 -0500

    2da API

---
+++ flaskr/App.py
@@ -0,0 +1,17 @@
+from flaskr import create_app
+from .Modelos import db, Cancion
+
+app = create_app('default')
+app_context = app.app_context()
+app_context.push()
+
+db.init_app(app)
+db.create_all()
+
+with app.app_context():
+    c = Cancion(titulo="Prueba",minutos=2, segundos=25, interprete="Diomedes")
+    db.session.add(c)
+    db.session.commit()
+    print(Cancion.query.all())
+
+
---
+++ flaskr/Modelos/modelos.py
@@ -0,0 +1,25 @@
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+
+class Cancion(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    titulo = db.Column(db.String(128))
+    minutos = db.Column(db.Integer)
+    segundos = db.Column(db.Integer)
+    interprete = db.Column(db.String(128))
+
+
+class Album(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    titulo = db.Column(db.String(128))
+    ano = db.Column(db.Integer)
+    descripcion = db.Column(db.String(128))
+    medio = db.Column(db.Medio)
+
+
+class Usuario(db.Model):
+    id = db.Column (db.Integer, primary_key=True)
+    nombre_usuario = db.Column(db.String(128))
+    Contraseña = db.Column(db.String(128))
